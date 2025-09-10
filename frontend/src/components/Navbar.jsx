801ce951e61b3f8dd2f4c3ecb7da32e145e6952b
Sifan Fita Hika <sifanfita0@gmail.com>
2025-09-10 08:39:48 +0300

    navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
         <div className='flex items-center gap-6'>
           <img src={assets.searchIcon} alt="search" className='w-5 cursor-pointer' />
           <div className='group relative'>
-            <img className='w-5 cursor-pointer' src={assets.profileIcon}></img>
+            <img className='w-5 cursor-pointer' src={assets.profileIcon}/>
             <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
               <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                 <p className='cursor-pointer hover:text-black'>My Profile</p>
