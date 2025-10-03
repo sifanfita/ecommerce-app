20e604bbc5e52fa45f681392f9b64bd18a4ea510
Sifan Fita Hika <sifanfita0@gmail.com>
2025-10-03 14:22:41 +0300

    list products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -72,7 +72,19 @@ const addProduct = async (req, res) => {
 // function for get all products
 const listProducts = async (req, res) => {
 
-    
+    try {
+        const products = await productModel.find({});
+        res.json({
+            success: true,
+            data: products
+        });
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        });
+    }
 
 }
 
