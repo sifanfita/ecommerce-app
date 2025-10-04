9859904e6e73c0caf2ecce601fb56e5ad65d740a
Sifan Fita Hika <sifanfita0@gmail.com>
2025-10-04 11:08:06 +0300

    side bar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import Sidebar from './component/Sidebar'
 
 
 const App = () => {
   return (
-    <>
-    <Navbar />
-    </>
+    <div className='bg-gray-50 min-h-screen'>
+      <>
+      <Navbar />
+      <hr />
+      <div className='flex w-full'>
+        <Sidebar />
+      </div>
+      </>
+    
+    </div>
   )
 }
 
